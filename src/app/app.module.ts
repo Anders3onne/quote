1882cbdae46909a8848250a3d55c1525a4d2f80b
Uwineza Andersonne <uwinezaandersonne@gmail.com>
2019-01-29 12:51:32 +0200

    Quotes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,13 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
-
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { QuoteComponent } from './quote/quote.component';
-import { QuoteDetailsComponent } from './quote-details/quote-details.component';
-import { StrikethroughDirective } from './strikethrough.directive';
-import { DateCountPipe } from './date-count.pipe';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { FormsModule } from "@angular/forms";
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { QuoteComponent } from "./quote/quote.component";
+import { QuoteDetailsComponent } from "./quote-details/quote-details.component";
+import { StrikethroughDirective } from "./strikethrough.directive";
+import { DateCountPipe } from "./date-count.pipe";
+import { GoalFormComponent } from "./goal-form/goal-form.component";
 
 @NgModule({
   declarations: [
@@ -14,13 +15,11 @@ import { DateCountPipe } from './date-count.pipe';
     QuoteComponent,
     QuoteDetailsComponent,
     StrikethroughDirective,
-    DateCountPipe
+    DateCountPipe,
+    GoalFormComponent
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, FormsModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
